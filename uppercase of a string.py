bcfce93efcf225966d8fc0966d3e428bf7a89597
Narmada G <99264206+clever-insighter@users.noreply.github.com>
2022-02-12 17:36:08 +0530

    Add files via upload

---
+++ uppercase of a string.py
@@ -0,0 +1,27 @@
+# now write the program captilize of a string
+
+
+def uppercaseof(anystring):
+    return anystring.upper()
+print(uppercaseof('python'))
+
+
+# captilize of a  string
+
+def uppercase(string):
+	return string.upper()
+print(uppercaseof("Pandas"))
+
+
+# captilize of a string
+
+def upper(string):
+	return string.upper()
+print(upper("Data Science"))	
+
+# program of a string captilize 
+
+def fun():
+	string = ("Tableau")
+	return string.upper()
+print(upper("Tableau"))
