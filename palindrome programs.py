964eb30466bce745c9440b56b7199ea77a34701a
Narmada G <99264206+clever-insighter@users.noreply.github.com>
2022-02-12 18:00:08 +0530

    Add files via upload

---
+++ palindrome programs.py
@@ -0,0 +1,21 @@
+
+
+# write a program of a palindrome number
+
+n =("Enter a number")
+a = (n[::-1])
+if n==a:
+    print("given number is palindrome")
+else:
+    print("given number is not a palindrome")
+   
+
+
+# write a  program if a  sequence is a palindrome
+
+a =("enter  sequence")
+b = a[::-1]
+if a == b:
+	print("Palindrome")
+else:
+	print("Not a palindrome")
