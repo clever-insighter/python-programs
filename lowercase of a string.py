aacc4cd32fa78e7b622ef6975c24e239204e9820
Narmada G <99264206+clever-insighter@users.noreply.github.com>
2022-02-12 17:37:22 +0530

    Add files via upload

---
+++ lowercase of a string.py
@@ -0,0 +1,26 @@
+# write a program of a lower case of a string
+
+def lowercaseof(anystring):
+    return anystring.lower()
+print(lowercaseof('PYTHON'))
+
+
+#  lower case of a string
+
+def lowercaseof(anystring):
+    return anystring.lower()
+print(lowercaseof('PANDAS'))
+
+# write a program of a string
+
+def lowercaseof(anystring):
+    return anystring.lower()
+print(lowercaseof('DATA SCIENCE'))
+
+
+# program of a string in lowercase
+
+def fun():
+	string = ("TABLEAU")
+	return string.lowercaseof()
+print(lowercaseof("TABLEAU"))
