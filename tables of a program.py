5ae871ced07e2f124ba83ac5e2c397b6a909cb67
Narmada G <99264206+clever-insighter@users.noreply.github.com>
2022-02-12 17:38:52 +0530

    Add files via upload

---
+++ tables of a program.py
@@ -0,0 +1,34 @@
+# write a program of a tables
+
+def tables():
+    for i in range(1,11):
+        print(i)
+        for j in range(1,11):
+            print(i,'*',j,'=',i*j)
+tables()       
+
+
+# write a porgram of a table print 1 and 5 table
+
+def tables():
+    for i in range(1,11):
+        print("_")
+        for j in range(1,11):
+            print(i,'*',j,'=',i*j)
+tables() 
+
+# write a program of table print 2 table
+
+def table():
+	n = 2
+	for i in range(1,11):
+		print(n,"x",i,"=",n*i)
+table()	
+
+# write a program of a 12 table
+
+def table():
+	n = 12
+	for i in range(1,11):
+		print(n,"x",i,"=",n*i)
+table()		
