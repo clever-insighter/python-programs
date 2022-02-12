dafd105cf2a5900e760f81a40587e13384ea9c4d
Narmada G <99264206+clever-insighter@users.noreply.github.com>
2022-02-12 17:37:54 +0530

    Add files via upload

---
+++ common letters of a list.py
@@ -0,0 +1,34 @@
+
+# program of a common letters of a list
+
+def f():
+    list1=[1,2,3,4]
+    list2=[4,5,2,6]
+    for x in list1:
+        for y in list2:
+            if(x==y):
+                print("the common elements are:", x)
+f()  
+
+
+# write a program of a common of a list
+
+def f():
+    list1=["Apple","Banana","Cherry"]
+    list2=["Mango","Cherry","Guava"]
+    for x in list1:
+        for y in list2:
+            if(x==y):
+                print("the common elements are:", x)
+f()  
+
+ # common strings of a list
+
+def f():
+    list1=["Pandas","Python","Numpy","Sql"]
+    list2=["Sql","Data Science","Python","Tableau"]
+    for x in list1:
+        for y in list2:
+            if(x==y):
+                print("the common elements are:", x)
+f()  
